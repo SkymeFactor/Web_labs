b2931639f6fcd0074e53c90d5d85735b96231e24
SkymeFactor <sergei.51351@yandex.ru>
2020-03-24 11:57:40 +0300

    Update task2.js

--- Lab_3/task2.js
+++ Lab_3/task2.js
@@ -12,3 +12,15 @@ function sum(set) {
 console.log(sum.call(this, [5, 5, 5, 5, 5, 5, 5, 5, 5]));
 
 document.getElementById("sum").innerHTML = sum.call(this, [5, 5, 5, 5, 5, 5, 5, 5, 5]);
+
+//Second variant:
+var sum = {
+    call : (set) => {
+        var total = 0;
+        
+        for (var i in set) {
+            total += Number(set[i]);
+        }
+        return total;
+    }
+}
