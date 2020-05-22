ba6866a89493ccdeefbc0b679083e2b9d01e6799
SkymeFactor <sergei.51351@yandex.ru>
2020-05-22 12:52:52 +0300

    Update auth.js

--- EndProject/src/auth.js
+++ EndProject/src/auth.js
@@ -15,7 +15,6 @@ const auth = {
         url += '&scope=' + init.scope
         url += '&response_type=' + init.responseType
         url += '&v=' + init.v
-        //this.url = init.redirectURI + '#access_token=2348c995ff33a38390d5fe6d7ccd7d1a575252a6e902a36e8b689bfc22fd3f76d89304211d7763bb72228&expires_in=86400&user_id=163781571&state=123456'
         console.log(url)
         sessionStorage.setItem('key_requested', true)
         window.location = url
@@ -36,16 +35,3 @@ const auth = {
 }
 
 export default auth
-
-//https://oauth.vk.com/authorize?client_id=5490057&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=friends&response_type=token&v=5.52
-
-/*let url = init.authLink + '?client_id=' + init.clientId
-url += '&display=' + init.displayType
-url += '&' + init.redirectURI
-url += '&scope=' + init.scope
-url += '&response_type' + init.responseType*/
-
-
-//'https://oauth.vk.com/authorize?client_id=163781571&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=photos&response_type=token'
-
-
